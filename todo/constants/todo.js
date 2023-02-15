eb7c41bd4836559c5511e0346b11df2cc2a64e3a
mess97 <101044639+mess97@users.noreply.github.com>
2023-02-16 00:03:40 +0900

    Delete todo directory

--- todo/constants/todo.js
+++
@@ -1,2 +0,0 @@
-export const TODO_COOKIE_KEY = "todoinfo";
-//상수 ->대문자로만구성
--- todo/main.js
+++
@@ -1,6 +0,0 @@
-import { changeState } from "./module/itemDOM.js";
-import { startTodoInfoDOM } from "./module/startDOM.js";
-import { getInputValue } from "./module/submitBtn.js";
-
-getInputValue();
-startTodoInfoDOM();
